fdff70331a4f06bcca9af03958f17a2791ff431b
Priyansh Patel <93965871+priyansh70@users.noreply.github.com>
2022-03-20 01:00:31 +0530

    Update 60-reduceMethod.js

--- 60-reduceMethod.js
+++ 60-reduceMethod.js
@@ -1,4 +1,5 @@
-// The arr.reduce() method in JavaScript is used to reduce the array to a single value and executes a provided function for each value of the array (from left-to-right) and the return value of the function is stored in an accumulator.
+// The arr.reduce() method in JavaScript is used to reduce the array to a single value and executes a provided function 
+// for each value of the array (from left-to-right) and the return value of the function is stored in an accumulator.
 
 // for every detail :-https://www.javascripttutorial.net/javascript-array-reduce/
 
